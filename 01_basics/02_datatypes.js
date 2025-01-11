1b9b1af00723e8ac678914429e5498437be89008
Sneha Mandal <techalita7070@gmail.com>
2025-01-11 15:39:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using nodeJS not browser 
+//it gives output 6 in browser ->inspect ->console
+
+console.log(3+3); console.log("Sneha") //not a good practice, code readability should be high
+
+console.log(3+3)
+console.log("Sneha")
+
+let name = "Sneha"
+let age = 18
+let isLoggedIn = false
+let state;      //undefined var
+let temp = null //null "stand alone value"
+
+    /*Primitive Data Types*/
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => 
+// symbol => unique
+
+//object
+console.log(typeof "Sneha");
+console.log(typeof age);
+console.log(typeof null);   //typeof null is object
+console.log(typeof undefined);  //typeof undefined is object
+
